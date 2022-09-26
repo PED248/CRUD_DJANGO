7b0ddb2d479aa8beb6fe8c2c11cab1e0e9c2bd39
PED248 <gepedromorocho@gmail.com>
2022-09-25 21:27:57 -0500

    feat:Create app chat

--- Crud/tasks/tests.py
+++ Crud/tasks/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from . models import task
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.tasks = task.objects.create(
+            title='django', description='django', important=False, user="1" "task.user")
+
+    def test_task_model(self):
+        dt = self.tasks
+        self.assertTrue(isinstance(dt, task))
